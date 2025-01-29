5bfc39e74cca146207c7a0a75ae6344f1d338d3c
Christian Machado Lopes <cfmlopes@hotmail.co.uk>
2025-01-29 20:09:36 +0000

    Webconfig update

--- src/main/java/com/mpk/config/WebConfig.java
+++ src/main/java/com/mpk/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.mpk.config;
+package com.mpk.controls.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,12 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
             .allowedOrigins("http://localhost:3000") // Your React app URL
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
-} 
+}
